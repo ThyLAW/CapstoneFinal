712854ad06bc29c796b3b6905f4f1db6362213bc
Logan <55511399+ThyLAW@users.noreply.github.com>
2021-11-17 14:22:40 -0500

    Add files via upload

---
+++ script.sh
@@ -0,0 +1,9 @@
+Response=$(curl -X POST http://127.0.0.1:5000/home --data "string=<script>alert('XSS')</script>")
+if [[ $? -gt 0 ]]
+then echo "Error connecting to website."
+else
+    if [[ ! -z $(echo ${Response} | grep "<script>alert('XSS')</script>") ]]
+    then echo "FAIL: XSS detected in website."
+    else echo "PASS: XSS mitigated in website."
+    fi
+fi
