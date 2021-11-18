fb025d64c4b650860b8d260ae6f16ec2a1b008d2
Logan <55511399+ThyLAW@users.noreply.github.com>
2021-11-17 20:40:30 -0500

    testing update index

--- test_app.py
+++
@@ -1,7 +0,0 @@
-import requests
-url = "http://localhost:5000/posts"
-mydata = "string=<script>alert('XSS!')</script>"
-
-x = requests.post(url)
-print(x.content)
-# curl -X POST http://127.0.0.1:5000/home --data "string=<script>alert('XSS!')</script>
