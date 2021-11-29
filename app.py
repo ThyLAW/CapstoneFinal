12d7b7562cf2763b3d791a6c01c871bb1b1e516b
Logan <55511399+ThyLAW@users.noreply.github.com>
2021-11-28 19:46:41 -0500

    fix vulnerabilities
    remove render template vuln
    remove debug = true
    add @app.after_request

--- app.py
+++ app.py
@@ -3,11 +3,16 @@
 from flask import Flask, request, url_for, render_template, redirect
 
 app = Flask(__name__, static_url_path='/static', static_folder='static')
-app.config['DEBUG'] = True
+app.config['DEBUG'] = False
 
 @app.route("/")
 def start():
     return render_template("index.html")
+    
+@app.after_request
+def apply_caching(response):
+    response.headers["X-Frame-Options"] = "SAMEORIGIN"
+    return response
 
 @app.route("/home", methods=['GET', 'POST'])
 def home():
