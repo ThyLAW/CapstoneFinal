163c8cdd36bf6c6618af7053c8b5d541dc7fc53b
Logan <55511399+ThyLAW@users.noreply.github.com>
2021-11-29 22:15:29 -0500

    Revert "Revert "Revert "Testing Fixes Don't Break Deploy"""

--- app.py
+++ app.py
@@ -3,39 +3,30 @@
 from flask import Flask, request, url_for, render_template, redirect
 
 app = Flask(__name__, static_url_path='/static', static_folder='static')
-app.config['DEBUG'] = False
+app.config['DEBUG'] = True
 
-
-@app.route("/", methods=['GET'])
+@app.route("/")
 def start():
     return render_template("index.html")
-
-
-@app.after_request
-def apply_caching(response):
-    response.headers["X-Frame-Options"] = "SAMEORIGIN"
-    response.headers['X-Content-Type-Options'] = 'nosniff'
-    return response
-
 
 @app.route("/home", methods=['GET', 'POST'])
 def home():
     if request.method == 'POST':
         xss = request.form['string']
-        return render_template("index.html", xss=xss)
+        return render_template("index.html",xss = xss)
     else:
         return redirect(url_for('start'))
 
 
-@app.route("/pathtraversal", methods=['GET', 'POST'])
+@app.route("/pathtraversal", methods=['GET','POST'])
 def pathtraversal():
     if request.method == 'POST':
         filename = request.form['filename']
         if filename == "":
             filename = "text/default.txt"
-        f = open(filename, 'r')
+        f = open(filename,'r')
         read = f.read()
-        return render_template("index.html", read=read)
+        return render_template("index.html", read = read)
     else:
         return redirect(url_for('start'))
 
@@ -43,7 +34,6 @@ def pathtraversal():
 @app.errorhandler(404)
 def page_not_found(e):
     return render_template("404.html")
-
 
 # test
 if __name__ == '__main__':
